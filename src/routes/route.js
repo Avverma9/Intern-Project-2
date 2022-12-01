4eda7d23aeecaa4e37e6b6e45702e9688cb4469e
AnkushRai-222 <ankushrai222@gmail.com>
2022-12-01 11:44:43 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -23,6 +23,7 @@ router.delete('/books/:bookId',middleWare.authenticate,bookController.deleteBook
 
 
 
+
 router.all('/*',function(req,res){
     return res.status(400).send({status:false,message:"invalid http request"})
 })
